090df8943d4b7a23a4c781598714ceb9a628c7d9
Vikram Panth <vikrampanth@gmail.com>
2021-08-17 00:18:58 +0530

    test

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="vikrampanth/mlapp"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+cat ~/environment/dockerdetail.txt | docker login --username vikrampanth --password-stdin
+docker tag mlappp4 $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
